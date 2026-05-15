4d4294f945a62b4fcb9dcbf718e6e56b4d61b3a0
iandol <iandol@cog5.org>
2026-05-15 15:24:41 +0800

    refactor: :zap: software moved to Cagelab-Code repository

--- software/scripts/cagelab-reset-OBS.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# a script to restart obs and mediamtx to fix streaming issues
-
-systemctl --user stop obs && systemctl --user stop mediamtx
-systemctl --user daemon-reload
-sleep 0.25s
-systemctl --user start mediamtx && systemctl --user start obs &
